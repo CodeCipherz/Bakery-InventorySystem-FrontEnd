d28c65e9f04e919fe206c92f35ceae6041c66256
Chamikara Bandara <chamikara38@gmail.com>
2023-12-07 23:07:11 +0530

    update by chami

--- Frontend/inc/footer.php
+++ Frontend/inc/footer.php
@@ -3,20 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
     <title>Footer</title>
 </head>
 <body>
     <div class="footer">
         <div class="f-cl-1">
-            <img src="logo.png" alt="logo">
+            <h2>LOGO</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br> Dolores minima ipsam illum aspernatur debitis </p>
             <h4>Follow us</h4>
             <img src="facebook.png" alt="Facebook">
             <img src="instagram.png" alt="Instagram">
         </div>
         <div class="f-cl-2">
-            <ul>
+            <ul=>
                 <li><a href="#">Birthday Cakes</a></li>
                 <li><a href="#">Wedding Cakes</a></li>
                 <li><a href="#">Anniversary Cakes</a></li>
