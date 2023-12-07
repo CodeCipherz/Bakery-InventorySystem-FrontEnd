d66941a35ba84e8f06b1010d785dc1a04e82b493
Chamikara Bandara <chamikara38@gmail.com>
2023-12-07 21:52:46 +0530

    Update by chami

---
+++ Frontend/inc/footer.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="footer">
+        <div class="f-cl-1">
+            <img src="logo.png" alt="logo">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores minima ipsam illum aspernatur debitis perspiciatis ipsum quaerat fugiat commodi distinctio dolor atque asperiores vel nam fuga, maxime officia reiciendis incidunt!</p>
+            <h6>Follow us</h6>
+        </div>
+    </div>
+</body>
+</html>
